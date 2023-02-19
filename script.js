4c1b76d6cc31a65c4b0273ac5baa0e1bd26119c2
Gukkon <shaunhomer1@hotmail.com>
2023-02-19 17:22:21 +0000

    javascript change

--- script.js
+++ script.js
@@ -1,73 +1,49 @@
-const firstName = 'Shaun';
 
-const lastName = 'Homer';
 
-const age = 14;
-
-const gender = 'Male';
-
-const pregnant = 'no';
-
-const howManyMonths = 0;
-
-//Pregnant length that will affect treatment
-if (howManyMonths <= 3) {
-    console.log('Restrictions will apply to what treatments you would like to have');
-} else {
-    console.log('Certain treatments will alter. Speak to your beautician for more details');
-  }
-
-//Age that will affect what treatments you can actually have
-if (age < 16) {
-    console.log('Please be advised that anyone under 16 cannot receive any treatment');
-} else if ((age === 16) || (age === 17)) {
-    console.log('Certain treatments will be unavailable to you.');
-} else {
-    console.log('Please choose from any treatment');
-}
+function fn1() {
 
 const massage = [
     {
         treatName: 'Full Body Massage',
         pregnant: false,
         duration: '1 Hour',
-        price: '£80',
+        price: 80,
     },
     {
         treatName: 'Full Deep Tissue Body Massage',
         pregnant: false,
         duration: '1.5 Hours',
-        price: '£120',
+        price: 120,
     },
     {
         treatName: 'Lower Body Massage',
         pregnant: true,
         duration: '30 Minutes',
-        price: '£40',
+        price: 40,
     },
     {
         treatName: 'Upper Body Massage',
         pregnant: false,
         duration: '30 Minutes',
-        price: '£40',
+        price: 40,
     },
     {
         treatName: 'Arms and Legs Massage',
         pregnant: false,
         duration: '1 Hour',
-        price: '£80',
+        price: 80,
     },
     {
         treatName: 'Pregnancy Massage',
         pregnant: true,
         duration: '1 Hour',
-        price: '£50',
+        price: 50,
     },
     {
         treatName: 'Soft Tissue Massage',
         pregnant: true,
         duration: '1 Hour',
-        price: '£70',
+        price: 70,
     },
 ];
 
@@ -199,3 +175,62 @@ const waxing = [
         price: '£80',
     },
 ];
+
+  if (massage.checked===true) {
+    console.log('You selected massage');
+} else if (facials.checked===true) {
+    console.log('You selected facials');
+} else if (manPed.checked===true) {
+  console.log('You selected manicures and pedicures');
+} else if (waxing.checked===true) {
+  console.log('You selected waxing');
+}
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+//Pregnant length that will affect treatment
+if (howManyMonths <= 3) {
+    console.log('Restrictions will apply to what treatments you would like to have');
+} else {
+    console.log('Certain treatments will alter. Speak to your beautician for more details');
+  }
+
+//Age that will affect what treatments you can actually have
+if (age < 16) {
+    console.log('Please be advised that anyone under 16 cannot receive any treatment');
+} else if ((age === 16) || (age === 17)) {
+    console.log('Certain treatments will be unavailable to you.');
+} else {
+    console.log('Please choose from any treatment');
+}
